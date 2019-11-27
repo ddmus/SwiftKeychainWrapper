7b588bd7ad69330e45c23b3268fa580d6363b4aa
Tomas Svoboda <hatchery41@icloud.com>
2019-11-27 10:51:42 +0100

    Updated package manifest by Alamofire

--- Package.swift
+++ Package.swift
@@ -1,15 +1,25 @@
-
 // swift-tools-version:5.0
+
+//import PackageDescription
+//
+//let package = Package(
+//    name: "SwiftKeychainWrapper",
+//    products: [
+//        .library(name: "SwiftKeychainWrapper", targets: ["SwiftKeychainWrapper"]),
+//    ],
+//    dependencies: [],
+//    targets: [
+//        .target(name: "SwiftKeychainWrapper", dependencies: [], path: "SwiftKeychainWrapper"),
+//    ]
+//)
+
 
 import PackageDescription
 
-let package = Package(
-    name: "SwiftKeychainWrapper",
-    products: [
-        .library(name: "SwiftKeychainWrapper", targets: ["SwiftKeychainWrapper"]),
-    ],
-    dependencies: [],
-    targets: [
-        .target(name: "SwiftKeychainWrapper", dependencies: [], path: "SwiftKeychainWrapper"),
-    ]
-)
+let package = Package(name: "SwiftKeychainWrapper",
+                      platforms: [.iOS(.v8)],
+                      products: [.library(name: "SwiftKeychainWrapper",
+                                          targets: ["SwiftKeychainWrapper"])],
+                      targets: [.target(name: "SwiftKeychainWrapper",
+                                        path: "SwiftKeychainWrapper")],
+                      swiftLanguageVersions: [.v5])
